5254f5cbf4a0e082f3136fd841b30bc0230ea04b
Caio Rosa <dev.caiorosa@gmail.com>
2026-05-25 21:29:09 +0100

    adicionar arquivos __init__.py e atualizar __all__ nas entidades e estruturas

---
+++ src/dominio/__init__.py
@@ -0,0 +1,5 @@
+"""Entidades do domínio do Sistema de Gestão de Linha de Autocarros."""
+ 
+from .passageiro import Passageiro
+ 
+__all__ = ["Passageiro"]
--- src/estruturas/__init__.py
+++ src/estruturas/__init__.py
@@ -1,4 +1,6 @@
 from .no import No
 from .lista_ligada import ListaLigada
+from .fila import Fila
+from .grafo import Grafo
 
-__all__ = ['No', 'ListaLigada']
+__all__ = ['No', 'ListaLigada', 'Fila', 'Grafo']
